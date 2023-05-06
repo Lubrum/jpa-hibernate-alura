5a885ba5cad8ec5c60e0c7d583f063fad644070f
Lubrum <lucianobrum18@gmail.com>
2023-05-06 18:34:19 -0300

    part iii, iv and v, all methods and lifecycle jpa/entitymanager

--- src/main/java/com/luciano/exemplo/CadastroDeProduto.java
+++ src/main/java/com/luciano/exemplo/CadastroDeProduto.java
@@ -1,23 +1,72 @@
 package com.luciano.exemplo;
 
+import com.luciano.exemplo.dao.CategoriaDao;
+import com.luciano.exemplo.dao.ProdutoDao;
+import com.luciano.exemplo.model.Categoria;
+import com.luciano.exemplo.model.Produto;
+import com.luciano.exemplo.util.JPAUtil;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
+
+import static java.lang.System.out;
 
 public class CadastroDeProduto {
-    public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("XIAomi Redmi");
-        celular.setDescricao("Legal");
-        celular.setPreco(new BigDecimal("800"));
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-        EntityManager em = factory.createEntityManager();
+    public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao dao = new ProdutoDao(em);
+
+        Produto p = dao.buscarPorId(1L);
+        out.println(p.getPreco());
+
+        List<Produto> produtos = dao.buscarTodos();
+        produtos.forEach(p1 -> System.out.println(p1.getNome()));
+
+        List<Produto> produtos2 = dao.buscarPorNome("XIAomi Redmi");
+        produtos2.forEach(out::println);
+
+        List<Produto> produtos3 = dao.buscarPorNomeDaCategoria("CELULARES");
+        produtos3.forEach(out::println);
+
+        BigDecimal preco = dao.buscarPrecoPorNome("XIAomi Redmi");
+        out.println("Preço = " + preco);
+    }
+
+    private static void cadastrarProduto() {
+
+        Categoria categoria = new Categoria("CELULARES");
+        Produto celular = new Produto("XIAomi Redmi", "Legal", new BigDecimal("800"), categoria);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+        ProdutoDao produtoDao = new ProdutoDao(em);
 
         em.getTransaction().begin();
-        em.persist(celular);
+
+        categoriaDao.cadastrar(categoria);
+        produtoDao.cadastrar(celular);
         em.getTransaction().commit();
         em.close();
+
+        // transient, managed, detached e removed, estados de entidade JPA/Hibernate
+        // transient: antes de persistir (nova entidade), managed: após dar um persist(), detached: após o clear() ou close()
+        // no entity manager, removed: entidade removida após delete, commit/flush, p. salvar no banco commit() ou flush(), e merge() para voltar entidade de detached
+        // para managed, find() e createQuery() para trazer do banco para 'managed'
+
+//        Produto celular2 = new Produto("teste Redmi", "Legal", new BigDecimal("800"), categoria);
+//        produtoDao.cadastrar(celular2);
+//        celular2.setNome("teste");
+//        em.flush();
+//        em.clear();
+//
+//        celular2 = em.merge(celular2);
+//        celular2.setNome("teste2");
+//        em.flush();
+//        em.remove(celular2);
+//        em.flush();
+//        em.close();
     }
 }
